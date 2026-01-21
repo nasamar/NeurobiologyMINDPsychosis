a0b7c92764d6b89f5e75a8035d58817a5f8bb4c4
Natalia García-San-Martín <132894441+nasamar@users.noreply.github.com>
2026-01-21 19:58:32 +0100

    Refactor brain map plotting function with updates

--- Code/regional_brainmap_representation.R
+++ Code/regional_brainmap_representation.R
@@ -8,18 +8,18 @@
 # 
 #  This file is part of Neurobiology MIND Psychosis toolkit.
 # 
-#  Neurobiology MIND Mapping toolkit is free software: 
+#  Neurobiology MIND Psychosis toolkit is free software: 
 #  you can redistribute it and/or modify it under the terms of the 
 #  GNU General Public License as published by the Free Software Foundation, 
 #  either version 3 of the License, or (at your option) any later version.
 # 
-#  Neurobiology MIND Mapping toolkit is distributed in the hope that 
+#  Neurobiology MIND Psychosis toolkit is distributed in the hope that 
 #  it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
 #  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 # 
 #  You should have received a copy of the GNU General Public License
-#  along with Neurobiology MIND Mapping toolkit. If not, see 
+#  along with Neurobiology MIND Psychosis toolkit. If not, see 
 #  <https://www.gnu.org/licenses/>.
 
 
@@ -119,3 +119,4 @@ regional_brainmap_representation <- function(v,text_Grap,sup_lim,inf_lim,midd_p)
     facet_wrap(~groups)+theme_brain2(plot.background = "white")
   
 }
+
