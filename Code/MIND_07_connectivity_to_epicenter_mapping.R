5d1a9c558410113b94a49bc97f2aa737fdcdbc01
Natalia García-San-Martín <132894441+nasamar@users.noreply.github.com>
2026-01-21 19:57:24 +0100

    Update script for plotting brain maps with epicenters

--- Code/MIND_07_connectivity_to_epicenter_mapping.R
+++ Code/MIND_07_connectivity_to_epicenter_mapping.R
@@ -9,18 +9,18 @@
 # 
 #  This file is part of Neurobiology MIND Psychosis toolkit.
 # 
-#  Neurobiology MIND Mapping toolkit is free software: 
+#  Neurobiology MIND Psychosis toolkit is free software: 
 #  you can redistribute it and/or modify it under the terms of the 
 #  GNU General Public License as published by the Free Software Foundation, 
 #  either version 3 of the License, or (at your option) any later version.
 # 
-#  Neurobiology MIND Mapping toolkit is distributed in the hope that 
+#  Neurobiology MIND Psychosis toolkit is distributed in the hope that 
 #  it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
 #  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 # 
 #  You should have received a copy of the GNU General Public License
-#  along with Neurobiology MIND Mapping toolkit. If not, see 
+#  along with Neurobiology MIND Psychosis toolkit. If not, see 
 #  <https://www.gnu.org/licenses/>.
 
 rm(list=ls())
@@ -51,3 +51,4 @@ for (i in epicenter){
   
   ggsave(paste0(location,'Data/connectivity_to_epicenters/plots/',i,'.png'), width = 8, height = 5, dpi = 300)
 }
+
